7176614c8851fb07da9cd57dc916d92d0ba0cebd
Thirawut <49267437+Lock-Lee@users.noreply.github.com>
2022-04-04 17:56:49 +0700

    Con

--- joke_meta/menu.ino
+++ joke_meta/menu.ino
@@ -1,42 +1,36 @@
 
-void settime(int start ) {
+void settime(int start)
+{
   lcd.setCursor(3, 0);
-  lcd.print((String)"SetTimeStart " + start);
+  lcd.print((String) "SetTimeStart " + start);
   lcd.setCursor(3, 2);
-  lcd.print((String)"SetTimeEnd " + start);
-
-
-
-
+  lcd.print((String) "SetTimeEnd " + start);
 }
-void setLux( ) {
+void setLux()
+{
   lcd.setCursor(0, 0);
   lcd.print("SetLux");
   lcd.setCursor(0, 2);
   lcd.print("To");
-
-
-
 }
-void settemp( ) {
+void settemp()
+{
   lcd.setCursor(0, 0);
   lcd.print("Set Temp");
   lcd.setCursor(4, 1);
-  lcd.print((String)"  To  ");
-
-
+  lcd.print((String) "  To  ");
 }
-void setstate( ) {
+void setstate()
+{
   lcd.setCursor(0, 0);
   lcd.print("0.Online");
   lcd.setCursor(0, 1);
   lcd.print("1.Manually");
   lcd.setCursor(4, 2);
   lcd.print("   ");
-
-
 }
-void mainmenu() {
+void mainmenu()
+{
 
   lcd.setCursor(3, 0);
   lcd.print("1 SET TIME 1 ");
@@ -46,5 +40,9 @@ void mainmenu() {
   lcd.print("3 SET LUX ");
   lcd.setCursor(3, 3);
   lcd.print("4 SET TEMP");
-
+}
+void setMode()
+{
+  lcd.setCursor(3, 0);
+  lcd.print("Set Mode ");
 }
